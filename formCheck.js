6857f04962d6553d54c7a385a819d43a3f18b47d
Maxith zhou <zhouyou457@hotmail.com>
2018-03-07 18:32:34 +0800

    form check plugin

---
+++ formCheck.js
@@ -0,0 +1,123 @@
+(function ($) {
+    var defaultSettings = {
+        'fields': [
+            {
+                'id': '',
+                'valids': [
+                    {
+                        'trigger': null,
+                        'func': function ($dom) {
+                        },
+                        'msg': 'error valid'
+                    }
+                ]
+            }
+        ]
+    };
+    var errorMsg = '<span class="help-block error-msg">#msg#</span>';
+    /**
+     * 方法列表
+     * @type {{init: init}}
+     */
+    var methods = {
+        init: function (options) {
+            var $this = $(this),
+                opt = $.extend({}, defaultSettings, options);
+            //初始化插件实例
+            var myPlugin = new MyPlugin();
+            myPlugin.$this = $this;
+            myPlugin.opt = opt;
+            //存储实例对象
+            $this.data('form_Check', myPlugin);
+            //添加事件监听
+            debugger
+            if (opt.fields.length > 0) {
+                for (var i = 0; i < opt.fields.length; i++) {
+                    var _item = opt.fields[i],
+                        $dom = $this.find('#' + _item.id);
+                    if ($dom[0]) {
+                        for (var j = 0; j < _item.valids.length; j++) {
+                            eventBind(_item.valids[j],$dom);
+                        }
+                    } else {
+                        $.error('id ' + _item.id + ' not found');
+                    }
+                }
+            }
+        },
+        check: function () {
+            var $this = $(this),
+                myPlugin = $this.data('form_Check');
+            console.log($this.data())
+            if (myPlugin == null){
+                $.error('plugin has not inited');
+                return false;
+            }
+            var opt = myPlugin.opt,
+                result = true;
+
+            //执行所有事件
+            if (opt.fields.length > 0) {
+                for (var i = 0; i < opt.fields.length; i++) {
+                    var _item = opt.fields[i],
+                        $dom = $this.find('#' + _item.id);
+                    if ($dom[0]) {
+                        for (var j = 0; j < _item.valids.length; j++) {
+                            var trigger = _item.trigger || 'form_check_trigger';
+                            $dom.trigger(trigger,function (flag) {
+                                if (!flag){
+                                    result = flag;
+                                }
+                            });
+                        }
+                    } else {
+                        $.error('id ' + _item.id + ' not found');
+                    }
+                }
+            }
+            return result
+        }
+    }
+    //事件绑定
+    var eventBind = function (_item, $dom) {
+        console.log('eventBind')
+        var trigger = _item.trigger || 'form_check_trigger';
+        $dom.on(trigger, function (event,_callback) {
+            console.log('trigger')
+            if (typeof _item.func !== 'undefined') {
+                var flag = _item.func($dom);
+                if (!flag) {
+                    setMessage(_item,$dom);
+                }
+                _callback(flag);
+            }
+        });
+    }
+    //设置消息
+    var setMessage = function (_item, $dom) {
+        var msg = errorMsg.replace(/#msg#/,_item.msg);
+        $dom.parent().append(msg);
+        $dom.parent().addClass('has-error');
+    }
+
+    //插件实例
+    MyPlugin.prototype = {
+        $this: null,
+        opt: null
+    }
+
+    function MyPlugin() {
+    }
+
+    //简易表单验证
+    $.fn.formCheck = function (method) {
+        // Method calling logic
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.formCheck');
+        }
+    }
+})(jQuery);
